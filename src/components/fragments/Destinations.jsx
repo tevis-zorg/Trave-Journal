9b8a52b92cb24017c236b6a4638639c654824311
tevis-zorg <tevisarifr@gmail.com>
2025-08-15 22:44:06 +0700

    See doc 15-08-2025

---
+++ src/components/fragments/Destinations.jsx
@@ -0,0 +1,75 @@
+import locMarker from '../../assets/location-marker.svg'
+
+const Destinations = (props) => {
+  return (
+    <>
+
+      <div className="destination-card">
+        {props.assetProps.map((data) => (
+          <div className="place-card" key={data.id}>
+
+            <img src={data.image} alt="" className="location" />
+
+            <span className="card-body">
+
+              <img src={locMarker} alt="" className="pin-loc" />
+
+              {data.destination}
+
+              <a href={data.location}>
+                View on Google Maps
+              </a>
+
+              <h2>
+                {data.destination}
+              </h2>
+
+              <p>
+                {data.description}
+              </p>
+
+            </span>
+
+          </div>
+
+        ))}
+      </div>
+
+
+      {/* <div className="place-card">
+      
+        <img className='location' src={image} alt={altImgName} />
+
+        <span className='card-body'>
+
+            <img className='pin-loc' src={markerLogo} alt="location marker" />
+            
+            {destName}
+
+            <a href={loc} className={destLoc}> 
+              View on Google Maps
+            </a>
+
+            <h2>
+              {destSite}
+            </h2>
+
+            <strong>
+              {schedule}
+            </strong>
+
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+              Similique aperiam a tenetur doloribus ipsa animi ullam! Impedit quis, 
+              dignissimos iusto, sequi iste esse, quod veniam repudiandae voluptate consequatur adipisci doloremque?
+            </p>
+            
+        </span>
+
+      </div> */}
+
+    </>
+  )
+}
+
+export default Destinations;
